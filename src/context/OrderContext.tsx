726cbbd63737006741efdb916810ae512cd64a13
hafidluqman50 <hafidluqmanulhakim@gmail.com>
2024-06-24 23:13:45 +0800

    refactor: refactoring loader context

--- src/context/OrderContext.tsx
+++ src/context/OrderContext.tsx
@@ -1,8 +1,8 @@
-import { ReactElement, ReactNode, createContext, useContext } from "react";
+import { ReactElement, ReactNode, createContext } from "react";
 import { Order } from "@/interfaces/Order";
 import { listOrders } from "@/lib/fakeData";
 
-const ListOrderContext = createContext<Order[]>([])
+export const ListOrderContext = createContext<Order[]>([])
 
 type ListOrderContextProps = {
   children: ReactNode
@@ -15,12 +15,4 @@ export function ListOrderContextProvider({children}: ListOrderContextProps): Rea
       {children}
     </ListOrderContext.Provider>
   )
-}
-
-export function useListOrdersAPI(): Order[] {
-  const context = useContext(ListOrderContext)
-  if (context === undefined) {
-    throw new Error("Context must be used within a Provider");
-  }
-  return context
 }
